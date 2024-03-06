15c23030a543194d1374b1a61f2f289f8e340b4c
rubydusa <rubydusa@protonmail.com>
2024-03-06 19:10:39 +0200

    updated generate_tests helper

--- circuits/generate_tests.ts
+++ circuits/generate_tests.ts
@@ -9,15 +9,14 @@ import { PathLike } from 'fs';
 
 import 'mocha';
 
-interface CircuitTest {
-    path: PathLike,
+interface CircuitTest { path: PathLike,
     name: string,
     cases: CircuitTestCase[]
 }
 
 interface CircuitTestCase {
-    input: object,
-    output: object | null,
+    input: object | Function | null,
+    output: object | Function | null,
     reasonOfFail?: string,
     description?: string
 }
@@ -26,30 +25,40 @@ export function generateCircuitTest(circuitTest: CircuitTest): void {
     describe(circuitTest.name, () => {
         let circuit: any;
         before(async () => {
-            circuit = await wasmTester(circuitTest.path);
+            circuit = await wasmTester(circuitTest.path, {compile: false});
         });
 
         for (const [i, caseData] of circuitTest.cases.entries()) {
-            const { input, output, reasonOfFail, description } = caseData;
+            const { reasonOfFail, description } = caseData;
+
             const caseName = description !== undefined ? `Case#${i}: ${description}` : `Case#${i}`; 
 
-            if (reasonOfFail !== undefined) {
-                it(caseName, async () => {
-                    await expect((async () => {
-                        const witness = await circuit.calculateWitness(input);
-                        await circuit.checkConstraints(witness);
-                    })()).to.be.rejectedWith(reasonOfFail);
-                });
-            } else {
-                it(caseName, async () => {
+            it(caseName, async () => {
+                const { input: rawInput, output: rawOutput } = caseData;
+                const input = typeof rawInput === 'function' 
+                    ? await rawInput()
+                    : rawInput;
+                const output = typeof rawOutput === 'function' 
+                    ? await rawOutput()
+                    : rawOutput;
+
+                const routine = async () => {
                     const witness = await circuit.calculateWitness(input);
                     await circuit.checkConstraints(witness);
 
                     if (output !== null) {
                         await circuit.assertOut(witness, output);
                     }
-                });
-            }
+                };
+
+                if (reasonOfFail !== undefined) {
+                    await expect(routine()).to.be.rejectedWith(reasonOfFail);
+                } else {
+                    await routine();
+                }
+            });
         }
     });
 }
+
+
